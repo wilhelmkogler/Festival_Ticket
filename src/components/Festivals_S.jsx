fd0712f3b2d4849652118634c9aa042a6d38ce37
Wilhelm Kogler <koglererikwilhelm754@gmail.com>
2025-05-16 23:09:25 +0200

    Minor filter and UI changes

--- src/components/Festivals_S.jsx
+++ src/components/Festivals_S.jsx
@@ -133,8 +133,13 @@ function Festival({ darkMode }) {
         darkMode ? "text-white" : "text-black"
       }`}
     >
-      <h1 className={`${darkMode ? "text-white" : "text-black"
-    } text-3xl lg:text-5xl font-bold mb-10 text-center`}>Upcoming Festivals</h1>
+      <h1
+        className={`${
+          darkMode ? "text-white" : "text-black"
+        } text-3xl lg:text-5xl font-bold mb-10 text-center`}
+      >
+        Upcoming Festivals
+      </h1>
 
       {loading ? (
         <p className="text-center">Loading...</p>
@@ -142,23 +147,26 @@ function Festival({ darkMode }) {
         <div className="flex flex-col lg:flex-row gap-10">
           {/* Filter sidebar */}
           <div className="lg:w-1/3 space-y-6">
-            <h2 className="text-xl font-semibold">Filter</h2>
-
             <div>
-              <label className="font-medium block mb-1">Sort by:</label>
               <select
-                className="w-full border px-2 py-1 rounded"
+                className="bg-white text-black w-full border px-3 py-2 rounded-full"
                 value={sortOption}
                 onChange={(e) => setSortOption(e.target.value)}
               >
-                <option value="upcoming">Start Date (Soonest First)</option>
-                <option value="latest">Start Date (Latest First)</option>
-                <option value="priceLow">Price (Lowest First)</option>
-                <option value="priceHigh">Price (Highest First)</option>
+                <option value="upcoming">
+                  Sorted by Start Date (Soonest First)
+                </option>
+                <option value="latest">
+                  Sorted by Start Date (Latest First)
+                </option>
+                <option value="priceLow">Sorted by Price (Lowest First)</option>
+                <option value="priceHigh">
+                  Sorted by Price (Highest First)
+                </option>
               </select>
             </div>
 
-            <div className="flex flex-col sm:flex-row gap-2">
+            <div className="flex flex-row-reverse gap-2">
               <button
                 onClick={applyFilters}
                 className="bg-blue-600 text-white px-4 py-2 rounded-full hover:bg-blue-700 w-full"
@@ -173,20 +181,24 @@ function Festival({ darkMode }) {
               </button>
             </div>
 
-            <p className="text-sm text-center font-medium mb-4">
+            <p className="text-lg text-center font-medium mb-4">
               Showing {filtered.length} festivals
             </p>
             <div className="flex justify-between flex-row-reverse gap-4">
               <div>
-                <p className="font-medium mb-1">Country</p>
-                <div className="flex flex-wrap gap-2">
+                <p className="font-medium mb-1">Country</p> <hr />
+                <div className="flex flex-wrap mt-2 gap-2">
                   {allCountries.map((country) => (
                     <button
                       key={country}
                       onClick={() => toggleFilterValue("countries", country)}
-                      className={`px-3 py-1 rounded-full border text-sm ${
+                      className={`px-3 py-1 rounded-full border-2 text-md hover:scale-125 transition-all duration-300 ${
                         filters.countries.includes(country)
-                          ? "bg-black text-white"
+                          ? "bg-orange-500 text-black"
+                          : "text-black"
+                      } ${
+                        darkMode
+                          ? "bg-transparent text-white "
                           : "bg-white text-black"
                       }`}
                     >
@@ -197,15 +209,19 @@ function Festival({ darkMode }) {
               </div>
 
               <div>
-                <p className="font-medium mb-1">Genre</p>
-                <div className="flex flex-wrap gap-2">
+                <p className="font-medium mb-1">Genre</p> <hr />
+                <div className="flex flex-wrap mt-2 gap-2">
                   {allGenres.map((g) => (
                     <button
                       key={g}
                       onClick={() => toggleFilterValue("genres", g)}
-                      className={`px-3 py-1 rounded-full border text-sm ${
+                      className={`px-3 py-1 rounded-full border-2 text-md hover:scale-125 transition-all duration-300 ${
                         filters.genres.includes(g)
-                          ? "bg-black text-white"
+                          ? "bg-orange-500 text-black"
+                          : "text-black"
+                      } ${
+                        darkMode
+                          ? "bg-transparent text-white "
                           : "bg-white text-black"
                       }`}
                     >
@@ -217,14 +233,18 @@ function Festival({ darkMode }) {
             </div>
 
             <div>
-              <p className="font-medium">Year</p>
-              <div className="flex flex-wrap gap-2">
+              <p className="font-medium">Year</p> <hr />
+              <div className="flex flex-wrap mt-2 gap-2">
                 {allYears.map((year) => (
                   <button
                     key={year}
-                    className={`px-3 py-1 rounded-full border ${
+                    className={`px-3 py-1 rounded-full border-2 text-md hover:scale-125 transition-all duration-300 ${
                       filters.years.includes(year)
-                        ? "bg-black text-white"
+                        ? "bg-orange-500 text-black"
+                        : "text-black"
+                    } ${
+                      darkMode
+                        ? "bg-transparent text-white "
                         : "bg-white text-black"
                     }`}
                     onClick={() => toggleFilterValue("years", year)}
@@ -236,14 +256,18 @@ function Festival({ darkMode }) {
             </div>
 
             <div>
-              <p className="font-medium">Month</p>
-              <div className="grid grid-cols-3 gap-2">
+              <p className="font-medium">Month</p> <hr />
+              <div className="grid grid-cols-3 mt-2 gap-2">
                 {monthNames.map((month) => (
                   <button
                     key={month}
-                    className={`px-4 py-2 rounded-full border text-sm ${
+                    className={`px-2 py-2 rounded-full border-2 text-md hover:scale-125 transition-all duration-300 ${
                       filters.months.includes(month)
-                        ? "bg-black text-white"
+                        ? "bg-orange-500 text-black"
+                        : "text-black"
+                    } ${
+                      darkMode
+                        ? "bg-transparent text-white "
                         : "bg-white text-black"
                     }`}
                     onClick={() => toggleFilterValue("months", month)}
@@ -253,27 +277,28 @@ function Festival({ darkMode }) {
                 ))}
               </div>
             </div>
-
             <div>
-              <p className="font-medium">Basic Price (€)</p>
-              <div className="flex gap-4">
-
+              <p className="font-medium">Price (€)</p> <hr />
+              <div className="flex mt-2 gap-4">
                 <input
-                type="number"
-                name="priceMin"
-                placeholder="Min"
-                className="border rounded w-1/2 px-2 py-1"
-                onChange={handlePriceChange}
-              />
-              <input
-                type="number"
-                name="priceMax"
-                placeholder="Max"
-                className="border rounded w-1/2 px-2 py-1"
-                onChange={handlePriceChange}
-              />
+                  type="number"
+                  name="priceMin"
+                  placeholder="Min"
+                  className={`${
+                    darkMode ? "bg-transparent text-white" : "text-black"
+                  } border-2 font-bold text-lg rounded w-1/2 px-2 py-1`}
+                  onChange={handlePriceChange}
+                />
+                <input
+                  type="number"
+                  name="priceMax"
+                  placeholder="Max"
+                  className={`${
+                    darkMode ? "bg-transparent text-white" : "text-black"
+                  } border-2 font-bold text-lg rounded w-1/2 px-2 py-1`}
+                  onChange={handlePriceChange}
+                />
               </div>
-              
             </div>
           </div>
 
@@ -291,21 +316,50 @@ function Festival({ darkMode }) {
                   alt={festival.name}
                   className="w-full h-48 object-cover rounded-lg mb-4"
                 />
-                <h2 className="text-2xl font-semibold">{festival.name}</h2>
-                <p className="text-sm text-gray-400">{festival.location}</p>
-                <p className="text-sm mt-2">{festival.description}</p>
-                <p className="text-sm">Tickets: {festival.ticketAvailable}</p>
-                <p className="text-lg font-bold">
-                  From: {festival.basicPrice} €
+                <div className="flex justify-between items-center text-xl lg:text-3xl font-bold">
+                  <p>{festival.name}</p>
+                  <p>
+                    {" "}
+                    {new Date(festival.dateStart).toLocaleDateString("en-US", {
+                      year: "numeric",
+                    })}
+                  </p>
+                </div>
+                <div className="mt-1 flex justify-between items-center text-md lg:text-xl font-semibold text-gray-400">
+                  <p>{festival.location}</p>
+                  <p>
+                    {new Date(festival.dateStart).toLocaleDateString("en-US", {
+                      month: "long",
+                      day: "numeric",
+                    })}
+                    {" - "}
+                    {new Date(festival.dateEnd).toLocaleDateString("en-US", {
+                      day: "numeric",
+                    })}
+                  </p>
+                </div>
+
+                <p className="text-sm lg:text-lg text-justify my-6">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Suscipit aspernatur cumque nam blanditiis nisi illum officiis
+                  rem iste, doloremque quae ducimus consectetur aperiam iure
+                  distinctio optio at quisquam nesciunt sint. Lorem ipsum dolor
+                  sit amet consectetur, adipisicing elit.
                 </p>
-                <p className="font-bold text-lg mt-8 text-center">
+
+                <div className="my-6 text-md lg:text-2xl font-semibold flex justify-around">
+                  <p>Tickets: {festival.ticketAvailable}</p>
+                  <p>Price: {festival.basicPrice} €</p>
+                </div>
+
+                <p className="font-bold text-md lg:text-2xl text-center">
                   Starts:{" "}
                   {new Date(festival.dateStart).toLocaleDateString("en-US", {
                     month: "long",
                     day: "numeric",
                   })}
                 </p>
-                <button className="w-full bg-green-500 text-white text-xl lg:text-3xl py-4 px-16 mt-4 rounded-full transition-all duration-150 hover:bg-green-600">
+                <button className="w-full bg-green-500 text-white text-2xl lg:text-3xl py-2 lg:py-4 mt-4 rounded-full transition-all duration-150 hover:bg-green-600">
                   Select
                 </button>
               </div>
